068d6ccfdf46d9f5c6d9cc89f32775df2fd01c85
George Jose <george.jose.a@gmail.com>
2017-11-09 01:24:17 -0500

    Nested comments now work

--- src/items/comment.js
+++ src/items/comment.js
@@ -4,42 +4,77 @@ import HTML from 'react-native-render-html';
 
 const Separator = () => <Text style={styles.metaText}> | </Text>;
 
-const Comment = props => {
-  const { _loading, text, by, kids, items } = props;
-  const numComments = (kids && kids.length) || 0;
-  if (!_loading) {
-    return (
-      <TouchableOpacity>
-        <View style={styles.container}>
-          <HTML html={text} />
-          <View style={styles.metaRow}>
-            <Text style={styles.metaText}>
-              {numComments} {numComments === 1 ? 'comment' : 'comments'}
-            </Text>
-            <Separator />
-            <Text style={styles.metaText}>by {by}</Text>
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { expanded: false };
+  }
+
+  componentDidMount() {
+    if (!this.props._loading && !this.props._loaded) {
+      this.props.fetchItemForId(this.props.id);
+    }
+  }
+
+  render() {
+    const { _loading, text, by, kids, items } = this.props;
+    const numComments = (kids && kids.length) || 0;
+    if (!_loading) {
+      return (
+        <TouchableOpacity
+          onPress={() => this.setState({ expanded: !this.state.expanded })}
+        >
+          <View style={styles.container}>
+            {text ? <HTML html={text} /> : <Text />}
+            <View style={styles.metaRow}>
+              <Text style={styles.metaText}>
+                {numComments} {numComments === 1 ? 'comment' : 'comments'}
+              </Text>
+              <Separator />
+              <Text style={styles.metaText}>by {by}</Text>
+            </View>
+            {this.state.expanded &&
+              kids &&
+              kids.map(
+                id =>
+                  items[id] ? (
+                    <Comment
+                      {...items[id]}
+                      key={id}
+                      items={items}
+                      fetchItemForId={this.props.fetchItemForId}
+                    />
+                  ) : (
+                    <Comment
+                      {...{ _loading: false, loaded: false, id }}
+                      key={id}
+                      fetchItemForId={this.props.fetchItemForId}
+                      items={items}
+                    />
+                  )
+              )}
           </View>
+        </TouchableOpacity>
+      );
+    }
+    return (
+      <View style={styles.container}>
+        <View style={styles.loadingTitle} />
+        <View style={styles.loadingTitle} />
+        <View style={styles.metaRow}>
+          <View style={styles.metaTextLoading} />
+          <Separator />
+          <View style={styles.metaTextLoading} />
         </View>
-      </TouchableOpacity>
+      </View>
     );
   }
-  return (
-    <View style={styles.container}>
-      <View style={styles.loadingTitle} />
-      <View style={styles.loadingTitle} />
-      <View style={styles.metaRow}>
-        <View style={styles.metaTextLoading} />
-        <Separator />
-        <View style={styles.metaTextLoading} />
-      </View>
-    </View>
-  );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     padding: 10,
-    marginLeft: 15,
+    marginLeft: 12,
     marginRight: 10,
   },
   commentText: {
