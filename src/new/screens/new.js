29687543a1d202e1f0f9e054ede585438c095d83
George Jose <george.jose.a@gmail.com>
2017-12-15 21:30:11 -0500

    Improved tabnavigator performance
    - Improved tabnavigator performance by not animating transitions between tabs
    - Having animations on and going from tab 1 -> tab n causes tabs 2, 3, 4, ... n-1 to also mount, significantly affecting performance

--- src/new/screens/new.js
+++ src/new/screens/new.js
@@ -4,18 +4,9 @@ import { Icon } from 'react-native-elements';
 import Stories from 'stories/stories';
 import { withStoryData } from 'stories/stories.utils';
 
-class New extends React.Component {
-  componentDidMount() {
-    console.log('Mounted New tab');
-  }
-  render() {
-    const { stories, fetchStories } = this.props;
-    return <Stories screenProps={{ stories, fetchStories }} />;
-  }
-}
-// const New = ({ stories, fetchStories }) => (
-//   <Stories screenProps={{ stories, fetchStories }} />
-// );
+const New = ({ stories, fetchStories }) => (
+  <Stories screenProps={{ stories, fetchStories }} />
+);
 
 New.navigationOptions = {
   tabBarLabel: 'New',
--- src/show/screens/show.js
+++ src/show/screens/show.js
@@ -4,19 +4,9 @@ import { Icon } from 'react-native-elements';
 import Stories from 'stories/stories';
 import { withStoryData } from 'stories/stories.utils';
 
-class Show extends React.Component {
-  componentDidMount() {
-    console.log('Mounted Show tab');
-  }
-  render() {
-    const { stories, fetchStories } = this.props;
-    return <Stories screenProps={{ stories, fetchStories }} />;
-  }
-}
-
-// const Show = ({ stories, fetchStories }) => (
-//   <Stories screenProps={{ stories, fetchStories }} />
-// );
+const Show = ({ stories, fetchStories }) => (
+  <Stories screenProps={{ stories, fetchStories }} />
+);
 
 Show.navigationOptions = {
   tabBarLabel: 'Show',
