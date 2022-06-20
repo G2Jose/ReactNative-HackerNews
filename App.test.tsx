e9def3e96f1fc29ad01f8d2ec01256800e0f89b6
George Jose <george.jose.a@gmail.com>
2022-06-19 21:54:31 -0400

    reset code

--- App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from '~/App';
-
-it('renders without crashing', () => {
-  jest.mock('ScrollView', () => jest.genMockFromModule('ScrollView')); // See https://github.com/facebook/react-native/issues/13034
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
--- ios/Pods/Target Support Files/appcenter-core/appcenter-core-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_appcenter_core : NSObject
-@end
-@implementation PodsDummy_appcenter_core
-@end
