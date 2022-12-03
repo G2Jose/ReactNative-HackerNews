948d840952d28d3581dec7de7d5d1329a4b13d79
George Jose <georgejose@Georges-MacBook-Pro.local>
2022-12-03 12:58:34 -0500

    Refactored - removed unnecessary files

--- src/common/store/epic.js
+++ src/common/store/epic.js
@@ -1,13 +1,5 @@
 import { combineEpics } from 'redux-observable';
-import topStoriesEpic from 'top/top.epic';
-import showStoriesEpic from 'show/show.epic';
-import newStoriesEpic from 'new/new.epic';
 import itemEpic from 'items/items.epic';
 
-const rootEpic = combineEpics(
-  topStoriesEpic,
-  showStoriesEpic,
-  newStoriesEpic,
-  itemEpic
-);
+const rootEpic = combineEpics(itemEpic);
 export default rootEpic;
