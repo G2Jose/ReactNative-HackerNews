011c38867adfe90a1a36fd0284c497684ca14781
George Jose <georgejose@Georges-MacBook-Pro.local>
2022-12-03 12:58:35 -0500

    Improved tabnavigator performance
    - Improved tabnavigator performance by not animating transitions between tabs
    - Having animations on and going from tab 1 -> tab n causes tabs 2, 3, 4, ... n-1 to also mount, significantly affecting performance

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ const Navigation = TabNavigator(
   },
   {
     tabBarPosition: 'bottom',
-    animationEnabled: true,
+    animationEnabled: false,
     lazy: true,
     tabBarOptions: {
       activeTintColor: '#000000',
--- src/top/screens/top.js
+++ src/top/screens/top.js
@@ -4,19 +4,9 @@ import { Icon } from 'react-native-elements';
 import Stories from 'stories/stories';
 import { withStoryData } from 'stories/stories.utils';
 
-class Top extends React.Component {
-  componentDidMount() {
-    console.log('Mounted Top tab');
-  }
-  render() {
-    const { stories, fetchStories } = this.props;
-    return <Stories screenProps={{ stories, fetchStories }} />;
-  }
-}
-
-// const Top = ({ stories, fetchStories }) => (
-//   <Stories screenProps={{ stories, fetchStories }} />
-// );
+const Top = ({ stories, fetchStories }) => (
+  <Stories screenProps={{ stories, fetchStories }} />
+);
 
 Top.navigationOptions = {
   tabBarLabel: 'Top',
