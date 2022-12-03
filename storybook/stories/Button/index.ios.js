8d137e85ad8a9f906f12663446bdac57b184e47b
George Jose <georgejose@Georges-MacBook-Pro.local>
2022-12-03 12:58:35 -0500

    Storybook support

---
+++ storybook/stories/Button/index.ios.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TouchableHighlight } from 'react-native';
+
+const Button = props => (
+  <TouchableHighlight onPress={props.onPress}>
+    {props.children}
+  </TouchableHighlight>
+);
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onPress: PropTypes.func,
+};
+Button.defaultProps = {
+  onPress: () => {},
+};
+
+export { Button as default };
