111e0d962899b93e6608dfe62789ca369490b7b9
George Jose <georgejose@Georges-MacBook-Pro.local>
2022-12-03 12:58:27 -0500

    Updated navigation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
 import NavigationBar from 'react-native-navbar';
 
-const Navbar = ({ title }) => <NavigationBar title={{title}} />
+const Navbar = ({ title }) => (
+	<NavigationBar
+		title={{
+			title,
+			style: styles.titleStyle,
+		}}
+		statusBar={{
+			tintColor: 'black',
+			style: 'light-content',
+		}}
+		tintColor="black"
+	/>
+);
+
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
 export default Navbar;
